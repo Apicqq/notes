7fd93c16d1fa380c28777d38297ea777cec97872
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2024-08-28 19:56:10 +0300

    feat: add DB configuration

---
+++ app/core/db.py
@@ -0,0 +1,36 @@
+from sqlalchemy import MetaData
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import declarative_base, declared_attr, sessionmaker
+
+from app.core.config import settings
+
+
+class PreBase:
+
+    @declared_attr
+    def __tablename__(cls):
+        return cls.__name__.lower()
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+
+Base = declarative_base(
+    cls=PreBase,
+    metadata=MetaData(
+        naming_convention={
+            "ix": "ix_%(column_0_label)s",
+            "uq": "uq_%(table_name)s_%(column_0_name)s",
+            "ck": "ck_%(table_name)s_`%(constraint_name)s`",
+            "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+            "pk": "pk_%(table_name)s",
+        }
+    ),
+)
+engine = create_async_engine(settings.database_url)
+AsyncSessionLocal = sessionmaker(engine)
+
+
+async def get_async_session() -> AsyncSession:
+    async with AsyncSessionLocal() as session:
+        yield session
