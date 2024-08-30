f476e84fb5172206c937685d2d0008865be4a473
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2024-08-30 14:38:42 +0300

    feat: add response preview in docs

--- app/api/endpoints/note.py
+++ app/api/endpoints/note.py
@@ -1,26 +1,26 @@
-from http import HTTPStatus
-
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.db import get_async_session
 from app.core.user import current_user
 from app.crud.notes import notes_crud
 from app.models import Note, User
-from app.schemas.notes import NoteBase, NoteCreate
-from app.services.spell_checker import check_spelling
+from app.schemas.notes import NoteCreate, NoteGet
 
 router = APIRouter()
 
 
 @router.get(
     "/",
-    response_model=list[NoteBase],
+    response_model=list[NoteGet],
 )
 async def get_notes_list(
         session: AsyncSession = Depends(get_async_session),
         user: User = Depends(current_user)
 ) -> list[Note]:
+    """
+    Вывести список заметок пользователя.
+    """
     notes = await notes_crud.get_notes_list_by_user(
         user=user,
         session=session
@@ -31,13 +31,27 @@ async def get_notes_list(
 @router.post(
     "/",
     response_model=NoteCreate,
+    responses={
+        400: {"description": "Spelling check failed",
+              "content": {
+                  "application/json": {
+                      "example": {
+                          "message": "Текст заметки не прошёл орфографическую проверку. Пожалуйста, исправьте ошибки в следующих словах:",
+                          "errors": [{"word1": "word", "suggestions": ["suggestion1", "suggestion2"]}]
+                      }
+                  }
+              }
+              }
+    }
 )
 async def create_note(
         note: NoteCreate,
         session: AsyncSession = Depends(get_async_session),
         user: User = Depends(current_user)
-):
-    spellchecking = check_spelling(note.content)
-    if spellchecking:
-        raise HTTPException(HTTPStatus.BAD_REQUEST, detail=spellchecking)
+) -> Note:
+    """
+    Создать новую заметку.
+    - **title**: Заголовок заметки — не более 100 символов.
+    - **content**: Содержание заметки — не более 255 символов.
+    """
     return await notes_crud.create(note, session=session, user=user)
