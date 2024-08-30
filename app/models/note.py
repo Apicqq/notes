05efd78392ad3618f62312bdc55ed499afb39b93
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2024-08-30 09:00:16 +0300

    feat: remove extra field from model

--- app/models/note.py
+++ app/models/note.py
@@ -1,5 +1,5 @@
 from sqlalchemy import String, Integer, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.orm import Mapped, mapped_column
 
 from app.core.base import *
 
@@ -7,5 +7,4 @@ from app.core.base import *
 class Note(Base):
     title: Mapped[str] = mapped_column(String(100))
     content: Mapped[str] = mapped_column(String(255))
-    author: Mapped["User"] = relationship(back_populates="notes")
     author_id: Mapped[int] = mapped_column(Integer, ForeignKey("user.id"))
