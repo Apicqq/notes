8e4b0151025b3fba7fcd0ecb26275ee8d52f6f16
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2024-08-31 12:31:33 +0300

    refactor: remove deprecated methods

--- app/main.py
+++ app/main.py
@@ -1,15 +1,24 @@
+from contextlib import asynccontextmanager, AbstractAsyncContextManager
 from http import HTTPStatus
 
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 
-from app.core.constants import ErrConstants as Err
 from app.api.routers import main_router
+from app.core.constants import ErrConstants as Err
 from app.core.exceptions import SpellingCheckException
 from app.core.init_db import create_first_superuser
 
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AbstractAsyncContextManager:
+    await create_first_superuser()
+    yield
+
+
 app = FastAPI(
     docs_url="/swagger",
+    lifespan=lifespan,
 )
 
 app.include_router(main_router)
@@ -17,7 +26,7 @@ app.include_router(main_router)
 
 @app.exception_handler(SpellingCheckException)
 async def spellcheck_exception_handler(
-    request: Request, exc: SpellingCheckException
+        request: Request, exc: SpellingCheckException
 ) -> JSONResponse:
     return JSONResponse(
         status_code=HTTPStatus.BAD_REQUEST,
@@ -31,8 +40,3 @@ async def spellcheck_exception_handler(
 @app.get("/")
 async def root() -> dict:
     return {"message": "please refer to /redoc or /swagger for documentation"}
-
-
-@app.on_event("startup")
-async def startup() -> None:
-    await create_first_superuser()
