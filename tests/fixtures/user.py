396c11f15d8cc185ae8adae34156d845ff44a2a7
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2024-08-31 11:36:34 +0300

    style: run black against code

--- tests/fixtures/user.py
+++ tests/fixtures/user.py
@@ -2,8 +2,12 @@ import pytest
 from fastapi.testclient import TestClient
 
 from app.models.user import User
-from tests.conftest import app, get_async_session, current_user, \
-    get_async_testing_session
+from tests.conftest import (
+    app,
+    get_async_session,
+    current_user,
+    get_async_testing_session,
+)
 
 user = User(
     id=1,
@@ -19,16 +23,22 @@ second_user = User(
     is_verified=True,
 )
 
+
 @pytest.fixture
 def user_client():
-    app.dependency_overrides = {get_async_session: get_async_testing_session,
-                                current_user: lambda: user}
+    app.dependency_overrides = {
+        get_async_session: get_async_testing_session,
+        current_user: lambda: user,
+    }
     with TestClient(app) as client:
         yield client
 
+
 @pytest.fixture
 def another_user_client():
-    app.dependency_overrides = {get_async_session: get_async_testing_session,
-                                current_user: lambda: second_user}
+    app.dependency_overrides = {
+        get_async_session: get_async_testing_session,
+        current_user: lambda: second_user,
+    }
     with TestClient(app) as client:
         yield client
--- tests/test_notes.py
+++ tests/test_notes.py
@@ -2,6 +2,7 @@ import pytest
 
 NOTES_URL = "/notes"
 
+
 def test_notes_list(notes_list, user_client):
     response = user_client.get(NOTES_URL)
     assert response.status_code == 200, (
@@ -20,6 +21,7 @@ def test_notes_list(notes_list, user_client):
         "пользователя (объект типа `list`)."
     )
 
+
 def test_notes_list_by_different_user(another_user_client, notes_list):
     response = another_user_client.get(NOTES_URL)
     assert response.status_code == 200, (
@@ -33,32 +35,29 @@ def test_notes_list_by_different_user(another_user_client, notes_list):
         "сделавшего запрос."
     )
 
-@pytest.mark.parametrize("json_data",
-                         [
-                             {"title": "Заголовок заметки",
-                              "content": "Содержание заметки"},
-                         ])
+
+@pytest.mark.parametrize(
+    "json_data",
+    [
+        {"title": "Заголовок заметки", "content": "Содержание заметки"},
+    ],
+)
 def test_notes_create_valid_data(user_client, json_data):
-    response = user_client.post(
-        "/notes",
-        json=json_data
-    )
+    response = user_client.post("/notes", json=json_data)
     assert response.status_code == 200, (
         "Корректный POST-запрос зарегистрированного пользователя к эндпоинту "
         f"`{NOTES_URL}` должен возвращать ответ со статус-кодом 200."
     )
 
 
-@pytest.mark.parametrize("json_data",
-                         [
-                             {"title": "Заголовок заметки",
-                              "content": "Текст с ашибками"},
-                         ])
+@pytest.mark.parametrize(
+    "json_data",
+    [
+        {"title": "Заголовок заметки", "content": "Текст с ашибками"},
+    ],
+)
 def test_notes_spellchecking_validation(user_client, json_data):
-    response = user_client.post(
-        "/notes",
-        json=json_data
-    )
+    response = user_client.post("/notes", json=json_data)
     assert response.status_code == 400, (
         "POST-запрос зарегистрированного пользователя к эндпоинту "
         f"`{NOTES_URL}`, содержащий ошибки правописания, "
@@ -69,17 +68,24 @@ def test_notes_spellchecking_validation(user_client, json_data):
 @pytest.mark.parametrize(
     "data, expected_status_code",
     [
-        ({"title": "Очень длинная заметка" * 100,
-          "content": "Содержание заметки"}, 422),
-        ({"title": "Заголовок заметки",
-          "content": "Очень длинная заметка" * 100}, 422),
-    ]
+        (
+            {
+                "title": "Очень длинная заметка" * 100,
+                "content": "Содержание заметки",
+            },
+            422,
+        ),
+        (
+            {
+                "title": "Заголовок заметки",
+                "content": "Очень длинная заметка" * 100,
+            },
+            422,
+        ),
+    ],
 )
 def test_notes_length_validation(user_client, data, expected_status_code):
-    response = user_client.post(
-        "/notes",
-        json=data
-    )
+    response = user_client.post("/notes", json=data)
     assert response.status_code == expected_status_code, (
         "Некорректный POST-запрос зарегистрированного пользователя"
         " к эндпоинту `{NOTES_URL}` должен возвращать ответ со статус-кодом "
