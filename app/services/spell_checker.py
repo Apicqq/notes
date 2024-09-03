bad0d6146efdcb929525afddb9f9faf4c3a64aec
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2024-09-03 22:54:31 +0300

    docs: add docstring to spellcheck function

--- app/services/spell_checker.py
+++ app/services/spell_checker.py
@@ -9,6 +9,15 @@ from app.core.config import settings
 def check_spelling(
     text: str, lang: str = "ru, en"
 ) -> list[dict[str, str]] | bool:
+    """
+    Проверка орфографии в тексте заметки.
+
+    Для осуществления проверки отправляется запрос на эндпоинт Яндекса,
+    после чего полученный ответ парсится в список ошибок и возвращается
+    пользователю.
+    :param text: Текст заметки.
+    :param lang: Язык текста, по умолчанию русский и английский.
+    """
     errors = list()
     try:
         payload = dict(text=text, lang=lang)
